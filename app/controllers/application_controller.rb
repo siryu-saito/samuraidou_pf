425ebe4442a1e06b1e3831d62bd9b6a93a6e6ee7
siryu-saito <saito.s.ryu@gmail.com>
2021-07-09 10:58:55 +0000

    devise機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:
-    [:last_name, :first_name, :last_name_kana, :first_name_kana, :prefectures])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :prefectures])
   end
 
 end
