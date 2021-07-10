8e340c4a7cae19f473429085c726e5bdf615674c
siryu-saito <saito.s.ryu@gmail.com>
2021-07-10 01:17:59 +0000

    Likeモデル   設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_005254) do
+ActiveRecord::Schema.define(version: 2021_07_10_011313) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "image_id", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
