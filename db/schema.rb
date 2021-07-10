9990e6886d913a7f673a66a399d98d1ca8a38fc9
siryu-saito <saito.s.ryu@gmail.com>
2021-07-10 01:04:14 +0000

    Postモデル設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_064739) do
+ActiveRecord::Schema.define(version: 2021_07_10_005254) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "text", null: false
+    t.string "place", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
