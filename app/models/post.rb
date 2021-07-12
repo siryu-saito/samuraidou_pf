c4ef34e480aa190b95db4159f368da2c251306df
siryu-saito <saito.s.ryu@gmail.com>
2021-07-12 01:53:14 +0000

    コメント機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
+  has_many :comments
   has_many :post_images, dependent: :destroy
+
   accepts_attachments_for :post_images, attachment: :image
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root to: 'homes#about'
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
 
-  devise_for :users
-
-  resources :posts
+  resources :posts do
+    
+    resources :comments, only: [:create]
+  end
 
 end
