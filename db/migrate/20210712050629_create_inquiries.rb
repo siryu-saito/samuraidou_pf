9336d4520d3a871b0d97800df1dbdaaf30cd163d
siryu-saito <saito.s.ryu@gmail.com>
2021-07-13 04:42:14 +0000

    カラム名の変更と変更に伴うレイアウトの一部変更

--- db/migrate/20210712050629_create_inquiries.rb
+++ db/migrate/20210712050629_create_inquiries.rb
@@ -1,9 +1,9 @@
 class CreateInquiries < ActiveRecord::Migration[5.2]
   def change
     create_table :inquiries do |t|
-      t.string :name
-      t.string :email
-      t.text :message
+      t.string :name,   null: false
+      t.string :email,    null: false
+      t.text   :message,  null: false
 
       t.timestamps
     end
