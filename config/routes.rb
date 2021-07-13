582e38a7148cf510bc09f15e5ebb6021d7ab60e3
siryu-saito <saito.s.ryu@gmail.com>
2021-07-13 11:58:40 +0000

    ログイン後ページの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'homes#about'
+  get 'homes/top'
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
 
