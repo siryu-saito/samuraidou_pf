0c7196dfb5225a580ecfcf489aab45fc5289c557
siryu-saito <saito.s.ryu@gmail.com>
2021-07-28 13:21:21 +0000

    投稿ユーザーのみが編集を可能にする

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -46,4 +47,10 @@ class PostsController < ApplicationController
                                  :rate, post_images_images: [])
   end
 
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user == current_user
+      redirect_to new_post_path
+    end
+  end
 end
